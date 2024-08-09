2f6210a75b3cc521f49fd711d94caa8b29251590
Sandra Sanders <sandy-sanders@msn.com>
2024-08-09 09:06:55 -0700

    added files for robots
    added files for robots

---
+++ src/card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const card = ({ name, email, id}) => {
+  return (
+    <div>
+      <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+        <img src={`https://robohash.org/${id}200x200`} alt="robots" />
+        <div>
+          <h2>{name}</h2>
+          <p>{email}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default card;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Card from './card';
+import './index.css';
+import "tachyons";
+import {robots} from './robots';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const root = createRoot(document.getElementById('root'));
+root.render(   
+  <StrictMode>
+    <div>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />;
+      
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />;
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />;
+
+    </div>
+  </StrictMode>
+  );
+
+  
